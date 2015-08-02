ae74d4f3acdd969fe3d1adcf827bf2280cad6d60
Alex Miles Younger <alex@alexmilesyounger.com>
2015-08-02 14:52:37 -0400

    Update files

---
+++ quiz.py
@@ -0,0 +1,10 @@
+import datetime
+import random
+
+from questions import Add, Multiply
+
+# a nice formatting habit is to list standard library imports at the very top and then list local custom imports a line down. This notation will let me easily see what is custom and what is standard as well as where modules are coming from
+
+
+class Quiz:
+	questions = []
