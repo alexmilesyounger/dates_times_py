d7c97bf669269a97dfe2fa1a0ce6094c92fe52a4
Alex Miles Younger <alex@alexmilesyounger.com>
2015-08-02 14:47:20 -0400

    Add commentary

--- wiki.py
+++ wiki.py
@@ -5,13 +5,15 @@ link_format = '%b_%d'
 link = 'https://en.wikipedia.org/wiki/{}'
 
 while True:
-	answer = input("What date would you like? Please use the MM/DD format. Enter 'quit' to quit.")
+	answer = input("What date would you like? Please use the MM/DD format. Enter 'quit' to quit. ")
 	if answer.upper() == 'QUIT':
 		break
 
 	try:
 		date = datetime.datetime.strptime(answer, answer_format)
+		# datetime.datetime.strptime() will qualify whether something is a valid date or not. I'm not sure how it's doing this, but it's just part of what is baked in. I kept thinking we'd have to build something complex to figure this out, but thank you Python for having done the hard work for me already.
 		output = link.format(date.strftime(link_format))
 		print(output)
 	except ValueError:
+		# how did he know that if the datetime.datetime.strptime() didn't find the date to be valid it would throw a ValueError? Experience. I guess I could have played around in the shell to figure that out. 
 		print("That's not a valid date. Please try again.")
