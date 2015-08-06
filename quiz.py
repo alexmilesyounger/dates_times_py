6f2ea0d8d730c6f528df0af78bc82824a5c91cb9
Alex Miles Younger <alex@alexmilesyounger.com>
2015-08-05 21:39:35 -0400

    Start building out the quiz

--- quiz.py
+++ quiz.py
@@ -11,8 +11,12 @@ class Quiz:
 	answers = []
 
 	def __init__(self):
-		# generate 10 random questions with numbers from 1 to 10
-		# add these questions into self.questions
+		question_types = (Add, Multiply)
+		for _ in range(10): # the underscore tells Python that we don't really care about the value we just care about doing it 10 time. 
+			num1 = random.randint(1, 10)
+			num2 = random.randint(1, 10)
+			question = random.choice(question_types)(num1, num2)
+			self.questions.append(question)
 
 	def take_quiz(self):
 		# log the start time
@@ -20,6 +24,7 @@ class Quiz:
 		# log if they got the question right
 		# log the end time
 		# show a summary
+		pass
 
 	def ask(self, question):
 		# log the start time
@@ -29,7 +34,17 @@ class Quiz:
 		# if the answer is right, send back True
 		# otherwise, send back False
 		# send back the elapsed time, too
+		pass
+
+	def total_correct(self):
+		total = 0
+		for answer in self.answers: # iterate through the list of answers
+			if answer[0]: # each answer is also a list, the first item of an answer's list is a Boolean, and if the Boolean is set to True then this if statement will increment the total by one
+				total += 1
+		return total
 
 	def summary(self):
-		# print how many you got right and the total # of questions. 5/10
-		# print the total time for the quiz: 30 seconds! 
+		print("You got {} out of {} right.".format(
+			self.total_correct(), len(self.questions)))
+		print("It took you {} seconds total.".format(
+			(self.end_time - self.start_time).seconds)) 
